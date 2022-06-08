235b4407b3855f68a401281aa9dd238dbc891ac7
Zhanar Akisheva <annaidym13@gmail.com>
2022-06-09 01:38:08 +0600

    add test

---
+++ src/main/java/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        // в строке не меньше 3 и не больше 19 символов,
+        // в строке есть только один пробел,
+        // пробел стоит не в начале и не в конце строки.
+        if (name.length() >= 3 && name.length() <= 19 && name.contains(" ") && name.indexOf(' ') == name.lastIndexOf(' ') && !name.startsWith(" ") && !name.endsWith(" ")){
+          return true;
+        } else {
+            return false;
+        }
+    }
+}
